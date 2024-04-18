c897ef06d2a388f949d03ca41826e8a6266da707
hy4ri <hiari45@gmail.com>
2024-04-19 00:38:48 +0300

    create

---
+++ .config/hypr/UserScripts/RofiBeats.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Directory for icons
+iDIR="$HOME/.config/swaync/icons"
+
+# Note: You can add more options below with the following format:
+# ["TITLE"]="link"
+
+# Define menu options as an associative array
+declare -A menu_options=(
+  ["Thunder ⛈️"]="https://soundcloud.com/relaxing-white-noise/epic-rain-and-thunder-5-hours"
+  ["Rain 🌧️ "]="https://soundcloud.com/relaxing-white-noise/rain-ocean-waves-storm-sounds-for-sleep-75-minutes"
+  ["WaterFall 🌊"]="https://soundcloud.com/relaxing-white-noise/relaxing-waterfall-sounds-for"
+)
+
+# Function for displaying notifications
+notification() {
+  notify-send -u normal -i "$iDIR/music.png" "Playing now: $@"
+}
+
+# Main function
+main() {
+  choice=$(printf "%s\n" "${!menu_options[@]}" | rofi -dmenu -config ~/.config/rofi/config-rofi-Beats.rasi -i -p "")
+
+  if [ -z "$choice" ]; then
+    exit 1
+  fi
+
+  link="${menu_options[$choice]}"
+
+  notification "$choice"
+  
+  # Check if the link is a playlist
+  if [[ $link == *playlist* ]]; then
+    mpv --shuffle --vid=no "$link"
+  else
+    mpv "$link"
+  fi
+}
+
+# Check if an online music process is running and send a notification, otherwise run the main function
+pkill mpv && notify-send -u low -i "$iDIR/music.png" "Online Music stopped" || main
---
+++ .config/hypr/scripts/KeyHints.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# Keyhints. Idea got from Garuda Hyprland
+
+# Detect monitor resolution and scale
+x_mon=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .width')
+y_mon=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .height')
+hypr_scale=$(hyprctl -j monitors | jq '.[] | select (.focused == true) | .scale' | sed 's/\.//')
+
+# Calculate width and height based on percentages and monitor resolution
+width=$((x_mon * hypr_scale / 100))
+height=$((y_mon * hypr_scale / 100))
+
+# Set maximum width and height
+max_width=1200
+max_height=1000
+
+# Set percentage of screen size for dynamic adjustment
+percentage_width=70
+percentage_height=70
+
+# Calculate dynamic width and height
+dynamic_width=$((width * percentage_width / 100))
+dynamic_height=$((height * percentage_height / 100))
+
+# Limit width and height to maximum values
+dynamic_width=$(($dynamic_width > $max_width ? $max_width : $dynamic_width))
+dynamic_height=$(($dynamic_height > $max_height ? $max_height : $dynamic_height))
+
+# Launch yad with calculated width and height
+yad --width=$dynamic_width --height=$dynamic_height \
+    --center \
+    --title="Keybindings" \
+    --no-buttons \
+    --list \
+    --column=Key: \
+    --column=Description: \
+    --column=Command: \
+    --timeout-indicator=bottom \
+" enter" "Terminal" "(kitty)" \
+" SHIFT Enter" "Sys Monitor" "(Btop)" \
+" Z" "App Launcher" "(rofi)" \
+" T" "Open File Manager" "(Thunar)" \
+" Q" "close active window" "(not kill)" \
+" Shift Q " "closes a specified window" "(window)" \
+" Alt V" "Clipboard Manager" "(cliphist)" \
+" W" "Choose wallpaper" "(Wallpaper Menu)" \
+"CTRL ALT W" "Random wallpaper" "(via swww)" \
+" B" "Hide/UnHide Waybar" "waybar" \
+" CTRL B" "Choose waybar styles" "(waybar styles)" \
+" ALT B" "Choose waybar layout" "(waybar layout)" \
+" ALT R" "Reload Waybar swaync Rofi" "CHECK NOTIFICATION FIRST!!!" \
+" SHIFT N" "Launch Notification Panel" "swaync Notification Center" \
+" Print" "screenshot" "(grim)" \
+" Shift Print" "screenshot region" "(grim + slurp)" \
+" Shift S" "screenshot region" "(swappy)" \
+"ALT Print" "Screenshot active window" "active window only" \
+"CTRL ALT P" "power-menu" "(wlogout)" \
+"CTRL ALT L" "screen lock" "(swaylock)" \
+"CTRL ALT Del" "Hyprland Exit" "(SAVE YOUR WORK!!!)" \
+" F" "Fullscreen" "Toggles to full screen" \
+" ALT L" "Toggle Dwindle | Master Layout" "Hyprland Layout" \
+" Shift F" "Toggle float" "single window" \
+" ALT F" "Toggle all windows to float" "all windows" \
+" Shift B" "Toggle Blur" "normal or less blur" \
+" SHIFT G" "Gamemode! All animations OFF or ON" "toggle" \
+" E" "View or EDIT Keybinds, Settings, Monitor" "" \
+" V" "Open Browser" "Vivaldi" \
+" G" "Open GamesLancher" "Cartridges" \
+" D" "Open Discord" "Vesktop" \
+
+
+
+
