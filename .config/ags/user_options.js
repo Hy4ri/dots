37c6eabb508b1afde3b8b5a7612ee34053086495
hy4ri <hiari45@gmail.com>
2024-05-05 16:38:44 +0300

    push

--- .config/ags/user_options.js
+++ .config/ags/user_options.js
@@ -6,8 +6,11 @@ const userConfigOptions = {
     //   options listed in this file will override the default ones in the above file
     // Here's an example
     'overview':{
-        'scale': 0.15,
-        'numOfRows': 2
+        'scale': 0.18,
+        'numOfRows': 3,
+        'numOfCols': 4,
+        'wsNumScale': 0.09,
+        'wsNumMarginScale': 0.07,
     },
     'keybinds': {
         'sidebar': {
