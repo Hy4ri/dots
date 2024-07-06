73a824a8aa0264a8b9b2a68a4bdae2ac14d4b444
hy4ri <hiari45@gmail.com>
2024-07-07 00:09:43 +0300

    paru

---
+++ scripts/paru.sh
@@ -0,0 +1,30 @@
+cat <<"EOF"
+
+██████╗  █████╗ ██████╗ ██╗   ██╗
+██╔══██╗██╔══██╗██╔══██╗██║   ██║
+██████╔╝███████║██████╔╝██║   ██║
+██╔═══╝ ██╔══██║██╔══██╗██║   ██║
+██║     ██║  ██║██║  ██║╚██████╔╝
+╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ 
+                                 
+EOF
+
+cd ~/
+
+check_paru_installed() {
+    if command -v paru &> /dev/null
+    then
+        git clone https://aur.archlinux.org/paru.git
+        cd paru
+        makepkg -si --noconfirm
+        sleep 1
+        paru -Syu --noconfirm
+    else
+        paru -Syu --noconfirm
+    fi
+}
+check_paru_installed
+
+clear
+sleep 1
+cd ~/dots
