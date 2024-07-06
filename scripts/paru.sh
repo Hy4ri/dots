b7ce1516fe10683708d08eca9831cb2cdd264790
hy4ri <hiari45@gmail.com>
2024-07-07 00:11:18 +0300

    s

--- scripts/paru.sh
+++ scripts/paru.sh
@@ -14,12 +14,12 @@ cd ~/
 check_paru_installed() {
     if command -v paru &> /dev/null
     then
+        paru -Syu --noconfirm
+    else
         git clone https://aur.archlinux.org/paru.git
         cd paru
         makepkg -si --noconfirm
         sleep 1
-        paru -Syu --noconfirm
-    else
         paru -Syu --noconfirm
     fi
 }
