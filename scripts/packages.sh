91349d943ac75cafcb423a54b4c97e591fbedeca
hy4ri <hiari45@gmail.com>
2024-07-07 00:19:22 +0300

    pkg

---
+++ scripts/packages.sh
@@ -0,0 +1,27 @@
+cat <<"EOF"
+
+██████╗  █████╗  ██████╗██╗  ██╗ █████╗  ██████╗ ███████╗███████╗
+██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔══██╗██╔════╝ ██╔════╝██╔════╝
+██████╔╝███████║██║     █████╔╝ ███████║██║  ███╗█████╗  ███████╗
+██╔═══╝ ██╔══██║██║     ██╔═██╗ ██╔══██║██║   ██║██╔══╝  ╚════██║
+██║     ██║  ██║╚██████╗██║  ██╗██║  ██║╚██████╔╝███████╗███████║
+╚═╝     ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝
+                                                                 
+
+EOF
+
+cd ~/
+
+paru -S --noconfirm bitwarden bat cartridges flatpak gammastep gamemode gparted heroic-games-launcher-bin hyprprop-git hyprshade-git lutris mangal-bin man-db man-pages qview scrcpy spotify-launcher timeshift thunar thunar-volman tumbler ffmpegthumbnailer file-roller thunar-archive-plugin vivaldi vivaldi-ffmpeg-codecs vivaldi-update-ffmpeg-hook vivaldi-widevine wev xwaylandvideobridge waypaper zathura zathura-cb zathura-djvu zathura-language-server zathura-pdf-mupdf zathura-ps zoxide
+
+sleep 2 
+
+paru -S --noconfirm aylurs-gtk-shell cliphist curl grim gvfs gvfs-mtp hypridle hyprlock hyprpaper imagemagick inxi jq foot kvantum nvim network-manager-applet pamixer pavucontrol pipewire-alsa playerctl polkit-gnome python-requests python-pyquery pyprland qt5ct qt6ct qt6-svg rofi-wayland slurp swappy swaync wallust-git waybar wget wl-clipboard wlogout xdg-user-dirs xdg-utils yad
+
+paru -S --noconfirm brightnessctl btop cava fastfetch vlc mpv mpv-mpris nvtop nwg-look pacman-contrib vim yt-dlp
+
+paru -R --noconfirm dunst mako
+
+clear
+sleep 1
+cd ~/dots
