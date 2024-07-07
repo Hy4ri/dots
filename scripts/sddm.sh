e98b3c8b70d042e6a1f8eeea7874dea66d2b1d0e
hy4ri <hiari45@gmail.com>
2024-07-08 00:28:50 +0300

    c

--- scripts/sddm.sh
+++ scripts/sddm.sh
@@ -12,7 +12,7 @@ EOF
 paru -S --noconfirm sddm qt6-5compat qt6-declarative qt6-svgsddm
 
 git clone https://github.com/stepanzubkov/where-is-my-sddm-theme
-cd where_is_my_sddm_theme
+cd where-is-my-sddm-theme
 sudo cp -r ~/dots/where_is_my_sddm_theme/ /usr/share/sddm/themes/
 echo "Successfully installed"
 if [[ $1 == "current" ]]; then
