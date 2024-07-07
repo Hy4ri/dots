a1a59f0f531173a28978abcbfda216fd1143b2cd
hy4ri <hiari45@gmail.com>
2024-07-08 00:07:16 +0300

    power

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -12,7 +12,7 @@ EOF
 
 cd ~/
 
-paru -S --noconfirm bitwarden bat cartridges flatpak gammastep gamemode gparted heroic-games-launcher-bin hyprprop-git hyprshade-git lutris mangal-bin man-db man-pages openrgb qview scrcpy spotify-launcher timeshift thunar thunar-volman tumbler ffmpegthumbnailer file-roller thunar-archive-plugin vivaldi vivaldi-ffmpeg-codecs vivaldi-update-ffmpeg-hook vivaldi-widevine wev xwaylandvideobridge waypaper zathura zathura-cb zathura-djvu zathura-language-server zathura-pdf-mupdf zathura-ps zoxide
+paru -S --noconfirm bitwarden bat cartridges flatpak gammastep gamemode gparted heroic-games-launcher-bin hyprprop-git hyprshade-git lutris mangal-bin man-db man-pages openrgb power-profiles-daemon qview scrcpy spotify-launcher timeshift thunar thunar-volman tumbler ffmpegthumbnailer file-roller thunar-archive-plugin vivaldi vivaldi-ffmpeg-codecs vivaldi-update-ffmpeg-hook vivaldi-widevine wev xwaylandvideobridge waypaper zathura zathura-cb zathura-djvu zathura-language-server zathura-pdf-mupdf zathura-ps zoxide
 
 sleep 2 
 
