c7e7df85ae08d9c7dd8c2358551dccf368a2ef59
hy4ri <hiari45@gmail.com>
2024-07-08 00:32:01 +0300

    sddm done

--- scripts/sddm.sh
+++ scripts/sddm.sh
@@ -13,11 +13,7 @@ paru -S --noconfirm sddm qt6-5compat qt6-declarative qt6-svgsddm
 
 git clone https://github.com/stepanzubkov/where-is-my-sddm-theme
 cd where-is-my-sddm-theme
-sudo cp -r ~/dots/where_is_my_sddm_theme/ /usr/share/sddm/themes/
-echo "Successfully installed"
-if [[ $1 == "current" ]]; then
-  sed -i 's/Current=.*/Current=where_is_my_sddm_theme/' /etc/sddm.conf.d/kde_settings.conf
-fi
+sudo ./install
 
 systemctl enable sddm.service --now
 
