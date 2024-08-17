c873a1e46267228082b9233cf27dfd26dd529696
hy4ri <hiari45@gmail.com>
2024-08-17 13:36:49 +0300

    Sway

--- .config/hypr/UserScripts/spotify-update.sh
+++ .config/hypr/UserScripts/spotify-update.sh
@@ -5,4 +5,3 @@ spicetify update
 curl -fsSL https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.sh | sh
 spicetify backup apply
 spicetify restore backup apply
-spotify-launcher 
