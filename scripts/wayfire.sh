81c8c5f2d3d0a9070d005e810e4138ee89b26061
hy4ri <hiari45@gmail.com>
2024-08-24 18:54:44 +0300

    feh

--- scripts/wayfire.sh
+++ scripts/wayfire.sh
@@ -5,7 +5,7 @@ $helper -Sy --noconfirm --needed wayfire wayfire-plugins-extra wcm wf-config wf-
 $helper -Sy --noconfirm --needed kitty kitty-shell-integration kitty-terminfo foot thunar file-roller rofi-wayland waybar \
 firefox network-manager-applet swaync swww gammastep blueman wl-clipboard mousepad lxapperance \
 network-manager-applet btop cliphist bat eza fastfetc galculato networkmanage neovim vlc mpv papirus-folders-git \
-papirus-icon-theme pavucontrol power-profiles-daemon ripgrep qview qt6ct kvantum thunar-archive-plugin \
+papirus-icon-theme pavucontrol power-profiles-daemon ripgrep feh qt6ct kvantum thunar-archive-plugin \
 thunar-volman waypaper wev wireplumber zoxide fzf wlr-randr swappy grim slurp
 
 sleep 1
