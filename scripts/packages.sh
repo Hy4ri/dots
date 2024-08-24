a2522ed22363ee0b45a8c007f20112633899fe8e
hy4ri <hiari45@gmail.com>
2024-08-24 19:07:58 +0300

    aur

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -4,7 +4,7 @@ $helper -Sy --noconfirm --needed alacritty bitwarden bat flatpak gammastep gamem
 papirus-icon-theme feh power-profiles-daemon scrcpy spotify-launcher timeshift thunar thunar-volman tumbler termdown \
 ffmpegthumbnailer file-roller thunar-archive-plugin wev xwaylandvideobridge xorg-host zathura zathura-cb zathura-djvu \
 zathura-language-server zathura-pdf-mupdf zathura-ps zoxide cliphist curl grim gvfs gvfs-mtp gvfs-google hypridle \
-hyprlock hyprpaper inxi jq foot kvantum nvim network-manager-applet pamixer pavucontrol pipewire-alsa playerctl \
+hyprlock hyprpaper inxi jq foot kvantum network-manager-applet pamixer pavucontrol pipewire-alsa playerctl \
 qt5ct qt6ct qt6-svg rofi-wayland slurp swappy swaync waybar wget wl-clipboard xdg-user-dirs xdg-utils yad brightnessctl \
 btop eza fastfetch vlc mpv mpv-mpris nvtop pacman-contrib neovim yt-dlp bash-completion bc bash-completion dua-cli duf \
 egl-wayland fish fish-autopair fish-pure-prompt fisher hwinfo intel-ucode lshw nano-syntax-highlighting obs-studio openssh \
