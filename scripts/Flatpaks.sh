96f200d04953b9d5a6569e6fead31029614e1531
hy4ri <hiari45@gmail.com>
2024-09-09 15:13:08 +0300

    update

--- scripts/Flatpaks.sh
+++ scripts/Flatpaks.sh
@@ -7,5 +7,8 @@ flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flath
 flatpak install --user -y flathub com.github.tchx84.Flatseal com.vivaldi.Vivaldi dev.vencord.Vesktop org.gimp.GIMP \
 org.onlyoffice.desktopeditors it.mijorus.smile com.visualstudio.code com.spotify.Client
 
+sudo flatpak override --filesystem=$HOME/.themes
+sudo flatpak override --filesystem=$HOME/.icons
+
 sleep 3
 cd ~/m57-dots-install
