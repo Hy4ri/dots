fdbdcbeb3233bb1861133a8927b9456abb9e4562
hy4ri <hiari45@gmail.com>
2024-11-15 22:27:12 +0300

    update

---
+++ .config/hypr/UserScripts/bakkasmod.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+WINEFSYNC=1 WINEPREFIX=/home/m57/Games/Heroic/Prefixes/default/Rocket\ League/pfx/ /home/m57/.config/heroic/tools/proton/Proton-GE-latest/files/bin/wine c:/Program\ Files/BakkesMod/BakkesMod.exe
