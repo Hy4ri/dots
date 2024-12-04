a5bf74be1574bc30587215878048953255b942af
M57 <41645758+Hy4ri@users.noreply.github.com>
2024-12-04 13:04:33 +0300

    Update hyprland.sh

--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -1,11 +1,12 @@
+
 #!/bin/bash
 
-$helper -Sy --noconfirm --needed hyprland hyprcursor xdg-desktop-portal-hyprland \
+$helper -Sy --needed hyprland hyprcursor xdg-desktop-portal-hyprland \
 hyprpaper hypridle hyprlock hyprlang hyprutils hyprwayland-scanner xwaylandvideobridge
 
-$helper -Sy --noconfirm --needed kitty kitty-shell-integration kitty-terminfo foot thunar file-roller rofi-wayland waybar \
+$helper -Sy --needed kitty kitty-shell-integration kitty-terminfo foot thunar file-roller rofi-wayland waybar \
 firefox network-manager-applet swaync swww gammastep blueman wl-clipboard mousepad nwg-look \
-network-manager-applet btop cliphist bat eza fastfetc galculato networkmanage neovim vlc mpv papirus-folders-git \
+network-manager-applet btop cliphist bat eza fastfetch networkmanager neovim vlc mpv papirus-folders-git \
 papirus-icon-theme pavucontrol power-profiles-daemon ripgrep feh qt6ct kvantum thunar-archive-plugin \
 thunar-volman waypaper wev wireplumber zoxide fzf wlr-randr swappy grim slurp ttf-jetbrains-mono-nerd
 
