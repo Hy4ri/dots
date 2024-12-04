2864065942709e6ccc6e0b43f4dd159b69f79db1
M57 <41645758+Hy4ri@users.noreply.github.com>
2024-12-04 12:58:32 +0300

    Update Flatpaks.sh

--- scripts/Flatpaks.sh
+++ scripts/Flatpaks.sh
@@ -5,7 +5,8 @@ sudo pacman -Sy --noconfirm --needed flatpak
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install --user -y flathub com.github.tchx84.Flatseal dev.vencord.Vesktop org.gimp.GIMP \
-org.onlyoffice.desktopeditors com.spotify.Client
+org.onlyoffice.desktopeditors com.spotify.Client com.rafaelmardojai.Blanket com.usebottles.bottles \
+io.github.flattool.Warehouse it.mijorus.gearlever \
 
 sudo flatpak override --filesystem=$HOME/.themes
 sudo flatpak override --filesystem=$HOME/.icons
