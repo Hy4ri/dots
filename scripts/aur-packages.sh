5a4ec5c125ad0aebb427c406f1beb68c9960be75
M57 <41645758+Hy4ri@users.noreply.github.com>
2024-12-04 13:02:35 +0300

    Update aur-packages.sh

--- scripts/aur-packages.sh
+++ scripts/aur-packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-$helper -Sy --noconfirm --needed cartridges heroic-games-launcher-bin hyprprop-git hyprshade-git mangal-bin \
-normcap papirus-folders-git qview spicetify-cli tty-clock vesktop waypaper aylurs-gtk-shell imagemagick cava \
-upscayl-bin visual-studio-code-bin tesseract-data-ara tesseract-data-eng
+$helper -Sy --needed cartridges heroic-games-launcher-bin mangal-bin \
+normcap papirus-folders-git qview spicetify-cli tty-clock vesktop hyprwall-bin imagemagick \
+upscayl-bin tesseract-data-ara tesseract-data-eng
 
 $helper -Sy --noconfirm --needed ds4drv game-devices-udev dualsensectl xone-dkms
 
