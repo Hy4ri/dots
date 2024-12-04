7e554fa577df7bbe2e6098f475dfd24b83c29eb6
M57 <41645758+Hy4ri@users.noreply.github.com>
2024-12-04 12:55:23 +0300

    Update Bluetooth.sh

--- scripts/Bluetooth.sh
+++ scripts/Bluetooth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$helper -Sy --noconfirm --needed gstreamer gst-libav gst-plugins-bad gst-plugins-base \
+$helper -Sy --needed gstreamer gst-libav gst-plugins-bad gst-plugins-base \
 gst-plugins-ugly gst-plugins-good libdvdcss alsa-utils alsa-firmware pavucontrol \
 lib32-pipewire-jack pipewire-support ffmpeg ffmpegthumbs ffnvcodec-headers bluez \
 bluez-utils bluez-plugins bluez-hid2hci bluez-cups bluez-libs bluez-tools \
