184eb4e71c004d8fad06fc458c35278b8a24adfb
hy4ri <hiari45@gmail.com>
2025-03-07 22:01:18 +0300

    new themes

--- scripts/pipewire.sh
+++ scripts/pipewire.sh
@@ -4,7 +4,7 @@ $helper -R --noconfirm pulseaudio pulseaudio-alsa pulseaudio-bluetooth
 
 systemctl --user disable --now pulseaudio.socket pulseaudio.service
 
-$helper -Sy --needed pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse
+$helper -Sy --needed pipewire wireplumber
 
 systemctl --user enable --now pipewire.socket pipewire-pulse.socket wireplumber.service
 systemctl --user enable --now pipewire.service
